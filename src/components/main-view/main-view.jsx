170f632308c19ff407273a67e7ae12b3367452bc
SwethaaSundar <swethaasundar@gmail.com>
2023-03-23 11:45:13 +0100

    Render Data from myFlix API into the Components

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,40 +1,15 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { MovieCard } from "../movie-card/movie-card";
 import { MovieView } from "../movie-view/movie-view";
 export const MainView = () => {
-  const [movies, setMovies] = useState([
-    {
-      id: 1,
-      title: "Snow White and the 7 Dwarfs",
-      description:
-        "The jealous Evil Queen decided to be rid of Snow White so that she would be fairest in the land, but the spell can be broken by True Love's Kiss.",
-      image:
-        "https://m.media-amazon.com/images/P/B00GK523GS.01._SCLZZZZZZZ_SX500_.jpg",
-      genre: "Fantasy",
-      director: "David Dodd Hand",
-    },
-    {
-      id: 2,
-      title: "Cinderella",
-      description:
-        "Cinderella is a dreamer who is trapped within a step-family who doesn't love or appreciate her. Enslaved by her evil stepmother and stepsisters, Cinderella dreams of going to the ball and meeting the Prince. With the help of a few mice friends and her fairy Godmother, Cinderella's dream comes true.",
-      image: "https://m.media-amazon.com/images/I/918LXIZ5JWL.jpg",
-      genre: "Fantasy",
-      director: "Kenneth Charles Branagh",
-    },
-    {
-      id: 3,
-      title: "Sleeping Beauty",
-      description:
-        "When Maleficent curses Princess Aurora at birth, the three good fairies hide her, but one of the faintest hopes is that with true love's kiss, the spell shall break.",
-      image:
-        "https://m.media-amazon.com/images/I/51xuD6F4LmL._SX412_BO1,204,203,200_.jpg",
-      genre: "Animation",
-      director: "Wolfgang Reitherman",
-    },
-  ]);
-
+  const [movies, setMovies] = useState([]);
+  useEffect(() => {
+    fetch("https://myflixdb-0sx9.onrender.com/movies")
+      .then((response) => response.json())
+      .then((data) => setMovies(data));
+  });
   const [selectedMovie, setSelectedMovie] = useState(null);
+
   if (selectedMovie) {
     return (
       <MovieView
--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -1,3 +1,6 @@
+//import the PropTypes library
+import PropType from "prop-types";
+
 export const MovieCard = ({ movie, onMovieClick }) => {
   return (
     <div
@@ -8,4 +11,23 @@ export const MovieCard = ({ movie, onMovieClick }) => {
       {movie.title}
     </div>
   );
+};
+//we define all the props constraints for the MovieCard
+MovieCard.protoTypes = {
+  movie: PropType.shape({
+    title: PropType.string.isRequired,
+    description: PropType.string.isRequired,
+    imgUrl: PropType.string.isRequired,
+    year: PropType.string.isRequired,
+    genre: PropType.shape({
+      gName: PropType.string.isRequired,
+      gDescription: PropType.string.isRequired,
+    }).isRequired,
+    director: PropType.shape({
+      dName: PropType.string.isRequired,
+      dBio: PropType.string.isRequired,
+      dYear: PropType.string.isRequired,
+    }).isRequired,
+  }).isRequired,
+  onMovieClick: PropType.func.isRequired,
 };
